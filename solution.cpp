b6f6a07a88592cefe0bb400d6e61da30fc7da68e
thomas <thomas@thomas-XPS-8700>
2014-11-14 17:16:55 +0800

    solution

---
+++ solution.cpp
@@ -0,0 +1,10 @@
+/*********************************************************************
+	* @file solution.h
+	* @author tangloner <tangloner@gmail.com>
+	* @date Fir Nov 14 2015
+	* @brief solution source file for leetcode questions which only 
+	need implement signal function
+**********************************************************************/
+#include "solution.h"
+
+
---
+++ solution.h
@@ -0,0 +1,18 @@
+/*********************************************************************
+	* @file solution.h
+	* @author tangloner <tangloner@gmail.com>
+	* @date Fir Nov 14 2015
+	* @brief solution header file for leetcode questions which only 
+	need implement signal function
+**********************************************************************/
+#include "header.h"
+
+
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+class Solution
+{
+	
+};
+#endif
